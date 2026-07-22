205062e482a355f5a36757cdd79fb77b286ba944
momen7alsmadi-droid <momen7alsmadi@users.noreply.github.com>
2026-07-23 02:41:54 +0300

    Fix race condition in index.js, add error logging to AntiCrash

--- Files〡[Resource]/Files〡[Handler]/AntiCrash.js
+++ Files〡[Resource]/Files〡[Handler]/AntiCrash.js
@@ -3,7 +3,11 @@
 * @param { import('discord.js').Client } Client
 */
 export default async function (Client) {
-  process.on('unhandledRejection', async function (Reason) { });
-  process.on('uncaughtException', async function (Reason) { });
+  process.on('unhandledRejection', async function (Reason) {
+    console.error('[AntiCrash] Unhandled Rejection:', Reason);
+  });
+  process.on('uncaughtException', async function (Reason) {
+    console.error('[AntiCrash] Uncaught Exception:', Reason);
+  });
   process.removeAllListeners('warning');
 }
--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 import Client from './Files〡[Resource]/Files〡[Client]/Client.js';
 const client = new Client();
 import { readdirSync } from 'fs';
-readdirSync('./Files〡[Resource]/Files〡[Handler]/').forEach((Handler) => {
-    import(`./Files〡[Resource]/Files〡[Handler]/${Handler}`).then(module => module.default(client));
+
+async function loadHandlers() {
+    const handlers = readdirSync('./Files〡[Resource]/Files〡[Handler]/');
+    await Promise.all(handlers.map(async (Handler) => {
+        const module = await import(`./Files〡[Resource]/Files〡[Handler]/${Handler}`);
+        await module.default(client);
+    }));
+}
+
+loadHandlers().then(() => {
+    client.login(client.Token);
+}).catch((err) => {
+    console.error('Failed to load handlers:', err);
+    client.login(client.Token);
 });
-client.login(client.Token)

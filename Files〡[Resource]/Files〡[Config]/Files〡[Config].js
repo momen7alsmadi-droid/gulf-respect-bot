153599b092590a193bbc6c917fe5bc790294897d
momen7alsmadi-droid <momen7alsmadi@users.noreply.github.com>
2026-07-24 16:47:07 +0300

    💬 v4.4.2 - شخصية درداشة: سولفجي، يفتح سالفة، بدون فلسفة
    - نبرة كلام أخويا: 'تعال ندردش' 'زي ما تتكلم مع اخوياك'
    - إيموجي خفيف بس 'لا تطرشر'
    - 'جاوب وخلص، لا تتفلسف ولا تشرح'
    - 'الي يستهبل اذبحه، الي يهايط قله زوط'

--- Files〡[Resource]/Files〡[Config]/Files〡[Config].js
+++ Files〡[Resource]/Files〡[Config]/Files〡[Config].js
@@ -1,7 +1,7 @@
 import { AttachmentBuilder } from "discord.js";
 
 // ! ========== المعرفات الأساسية ==========
-export const VERSION = '4.4.1';
+export const VERSION = '4.4.2';
 export const Founder = '1387331972094890036';
 export const Owners = ["1387331972094890036", "1154021789148659813"]
 export const GuildID = '1497265109247201280';
